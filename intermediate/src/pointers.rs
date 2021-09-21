cd0fd90f5a0668386704d61c899e569a346f7873
chris <christophpryer@gmail.com>
2021-09-20 22:39:09 -0400

    updated pointers.rs

--- intermediate/src/pointers.rs
+++ intermediate/src/pointers.rs
@@ -2,6 +2,12 @@
 const EXAMPLE_INT: u32 = 300;
 const EXAMPLE_REF: &u32 = &EXAMPLE_INT;
 
+fn add_to_ref(value: &u32, add_value: u32) -> u32 {
+    let new_value = *value + add_value;
+
+    return new_value;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +16,19 @@ mod tests {
     fn test_references() {
         // dereferencing should evaluate to true
         assert_eq!(EXAMPLE_INT, *EXAMPLE_REF);
+
+        // using functions to pass references to data around
+        let some_value = 200;
+        let some_other_value = 500;
+        let new_value = add_to_ref(&some_value, some_other_value);
+
+        assert_eq!(some_value + some_other_value, new_value);
+
+        // allowed to alias vairables if the ref is immmutable
+        let a = 'a';
+        let ref_a1 = &a;
+        let ref_a2 = &a;
+
+        assert_eq!(ref_a1, ref_a2);
     }
 }
