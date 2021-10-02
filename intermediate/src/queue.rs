143135ae6f061b5d16646dbe9877de122e91b0be
Chris <cnpryer@gmail.com>
2021-10-02 17:54:02 -0400

    added basic Queue

--- intermediate/src/queue.rs
+++ intermediate/src/queue.rs
@@ -1,9 +1,38 @@
 use ringbuf::RingBuffer;
 
-const BUFFER_SIZE: usize = 2;
+const RING_BUF_SIZE: usize = 2;
 
-fn create_queue() -> RingBuffer<i32> {
-    return RingBuffer::<i32>::new(BUFFER_SIZE);
+struct Queue<T> {
+    queue: Vec<T>,
+}
+
+impl<T> Queue<T> {
+    fn new() -> Self {
+        Queue { queue: Vec::new() }
+    }
+
+    fn length(&self) -> usize {
+        self.queue.len()
+    }
+
+    fn enqueue(&mut self, item: T) {
+        self.queue.push(item)
+    }
+
+    fn dequeue(&mut self) -> T {
+        self.queue.remove(0)
+    }
+    fn is_empty(&self) -> bool {
+        self.queue.is_empty()
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.queue.first()
+    }
+}
+
+fn create_ringbuf_queue() -> RingBuffer<i32> {
+    return RingBuffer::<i32>::new(RING_BUF_SIZE);
 }
 
 #[cfg(test)]
@@ -12,11 +41,24 @@ mod tests {
 
     #[test]
     fn test_queue() {
-        let queue = create_queue();
+        let mut queue: Queue<isize> = Queue::new();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+
+        let item = queue.dequeue();
+
+        assert_eq!(item, 1);
+        assert_eq!(queue.is_empty(), false)
+    }
+
+    #[test]
+    fn test_ringbuf_queue() {
+        let queue = create_ringbuf_queue();
 
         let (mut prod, mut cons) = queue.split();
 
-        // test up to BUFFER_SIZE
+        // test up to RING_BUF_SIZE
         prod.push(0).unwrap();
         prod.push(1).unwrap();
         assert_eq!(prod.push(2), Err(2));
