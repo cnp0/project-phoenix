584b66aef04abada3bfa33789c28bcac9dcc0196
Chris <cnpryer@gmail.com>
2021-10-25 21:35:02 -0400

    added basic command design pattern

--- intermediate/src/design_patterns/command.rs
+++ intermediate/src/design_patterns/command.rs
@@ -1,1 +1,18 @@
+trait Command {
+    fn new(data: String) -> SimpleCommand;
+    fn execute(&self);
+}
 
+struct SimpleCommand {
+    data: String,
+}
+
+impl Command for SimpleCommand {
+    fn new(data: String) -> SimpleCommand {
+        return SimpleCommand { data };
+    }
+
+    fn execute(&self) {
+        println!("Data: {}", self.data);
+    }
+}
