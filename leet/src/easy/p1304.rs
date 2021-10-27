adfbd33dad14a12ceb1ecb8b638f9d426d38ac94
Chris <cnpryer@gmail.com>
2021-10-26 20:53:18 -0400

    added leetcode problem 1304

---
+++ leet/src/easy/p1304.rs
@@ -0,0 +1,48 @@
+// Find N Unique Integers Sum up to Zero
+// Given an integer n, return any array containing n unique integers such that they add up to 0.
+//
+// Example:
+// Input: n = 5
+// Output: [-7,-1,1,3,4]
+// Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+//
+// Example:
+// Input: n = 3
+// Output: [-1,0,1]
+//
+// Example:
+// Input: n = 1
+// Output: [0]
+//
+// Constraints:
+// 1 <= n <= 1000
+struct Solution;
+
+impl Solution {
+    // Given 1 <= n <= 1000, return a vector of n-elements that sum to 0.
+    pub fn sum_zero(n: i32) -> Vec<i32> {
+        let mut vec: Vec<i32> = (0..n).collect();
+        let sum: i32 = vec.iter().sum();
+
+        vec[0] = vec[0] - sum;
+
+        return vec;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_NUMS: [i32; 5] = [5, 17, 31, 511, 999];
+
+    // TODO: add nunique check
+    #[test]
+    fn test_solution() {
+        for n in TEST_NUMS.iter() {
+            let vec = Solution::sum_zero(*n);
+            let sum: i32 = vec.iter().sum();
+            assert_eq!(sum, 0);
+        }
+    }
+}
---
+++ leet/src/lib.rs
@@ -0,0 +1,8 @@
+#[allow(dead_code)]
+pub mod easy;
+
+#[allow(dead_code)]
+pub mod medium;
+
+#[allow(dead_code)]
+pub mod hard;
