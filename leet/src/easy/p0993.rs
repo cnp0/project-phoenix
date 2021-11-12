73927a951a500b0d9d3074a401a96e9d9ed722dc
Chris <cnpryer@gmail.com>
2021-11-11 21:57:18 -0500

    added to leetcode problem 993

--- leet/src/easy/p0993.rs
+++ leet/src/easy/p0993.rs
@@ -44,6 +44,8 @@
 //     Each node has a unique value.
 //     x != y
 //     x and y are exist in the tree.
+
+// TODO: properly use std::rc::Rc and follow idomatic ownership
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -66,11 +68,56 @@ impl TreeNode {
     }
 }
 
-struct Solution;
+struct Solution {
+    vals: Vec<(Rc<RefCell<TreeNode>>, i32)>,
+}
 
 impl Solution {
-    pub fn is_cousins(_root: Option<Rc<RefCell<TreeNode>>>, _x: i32, _y: i32) -> bool {
-        return false;
+    pub fn new() -> Solution {
+        Solution { vals: Vec::new() }
+    }
+
+    pub fn is_cousins(&mut self, root: Option<Rc<RefCell<TreeNode>>>, x: i32, y: i32) -> bool {
+        self.search(&root, &None, &0, &x, &y);
+
+        let (node_x, node_y) = (&self.vals[0], &self.vals[1]);
+
+        return *node_x.0 != *node_y.0 && node_x.1 == node_y.1;
+    }
+
+    fn search(
+        &mut self,
+        node: &Option<Rc<RefCell<TreeNode>>>,
+        parent: &Option<Rc<RefCell<TreeNode>>>,
+        depth: &i32,
+        x: &i32,
+        y: &i32,
+    ) {
+        if node.is_none() {
+            return;
+        }
+
+        // check if delta between k and node.val exists in the set of all found
+        if node.as_ref().unwrap().borrow().val == *x || node.as_ref().unwrap().borrow().val == *y {
+            self.vals.push((node.as_ref().unwrap().clone(), *depth));
+        }
+
+        let new_depth = depth + 1;
+
+        self.search(
+            &node.as_ref().unwrap().borrow().left,
+            parent,
+            &new_depth,
+            x,
+            y,
+        );
+        self.search(
+            &node.as_ref().unwrap().borrow().right,
+            parent,
+            &new_depth,
+            x,
+            y,
+        );
     }
 }
 
